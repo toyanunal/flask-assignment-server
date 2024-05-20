4c979f229e918a4180c77fd7f8c06f9b93acb488
toyanunal <toyanunal@gmail.com>
2024-05-20 03:27:25 +0300

    Add custom assignment generator functionalities

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, session, request, jsonify, render_template_string, send_from_directory, redirect, url_for
+from flask import Flask, session, request, jsonify, render_template_string, redirect, url_for
 from flask_session import Session # Import Session for server-side sessions
 from datetime import datetime, timedelta
-import re, secrets, os, boto3, logging
+import re, secrets, os, boto3, logging,  tempfile, zipfile, shutil, hashlib, random
+from lxml import etree
 
 app = Flask(__name__)
 
@@ -28,6 +29,123 @@ def extract_number(text):
     numbers = re.findall(r'\d+', text)
     return numbers[0] if numbers else None
 
+def embed_hidden_info_docx(docx_path, ext_user_username, temp_dir, new_docx_path):
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    os.makedirs(temp_dir)
+
+    with zipfile.ZipFile(docx_path, 'r') as zip_ref:
+        zip_ref.extractall(temp_dir)
+
+    custom_xml_path = os.path.join(temp_dir, 'customXml/item1.xml')
+    hash_object = hashlib.sha256(ext_user_username[1:].encode())
+    hex_dig = hash_object.hexdigest()
+
+    root = etree.Element('root')
+    hidden_info = etree.SubElement(root, 'hiddenInfo')
+    hidden_info.text = hex_dig
+    tree = etree.ElementTree(root)
+
+    custom_xml_dir = os.path.dirname(custom_xml_path)
+    if not os.path.exists(custom_xml_dir):
+        os.makedirs(custom_xml_dir)
+
+    with open(custom_xml_path, 'wb') as xml_file:
+        tree.write(xml_file, xml_declaration=True, encoding='UTF-8')
+
+    with zipfile.ZipFile(new_docx_path, 'w') as zip_ref:
+        for folder_name, subfolders, filenames in os.walk(temp_dir):
+            for filename in filenames:
+                file_path = os.path.join(folder_name, filename)
+                arcname = os.path.relpath(file_path, temp_dir)
+                zip_ref.write(file_path, arcname)
+
+    shutil.rmtree(temp_dir)
+    return new_docx_path
+
+def embed_hidden_info_xlsx(xlsx_path, ext_user_username, temp_dir, new_xlsx_path):
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    os.makedirs(temp_dir)
+
+    with zipfile.ZipFile(xlsx_path, 'r') as zip_ref:
+        zip_ref.extractall(temp_dir)
+
+    workbook_xml_path = os.path.join(temp_dir, 'xl/workbook.xml')
+    tree = etree.parse(workbook_xml_path)
+    root = tree.getroot()
+
+    hash_object = hashlib.sha256(ext_user_username[1:].encode())
+    hex_dig = hash_object.hexdigest()
+    hidden_info = etree.Element('hiddenInfo')
+    hidden_info.text = hex_dig
+    root.append(hidden_info)
+
+    tree.write(workbook_xml_path, xml_declaration=True, encoding='UTF-8')
+
+    with zipfile.ZipFile(new_xlsx_path, 'w') as zip_ref:
+        for folder_name, subfolders, filenames in os.walk(temp_dir):
+            for filename in filenames:
+                file_path = os.path.join(folder_name, filename)
+                arcname = os.path.relpath(file_path, temp_dir)
+                zip_ref.write(file_path, arcname)
+
+    shutil.rmtree(temp_dir)
+    return new_xlsx_path
+
+def create_zip(ext_user_username, hw_number, temp_dir):
+    output_dir = tempfile.mkdtemp(dir=temp_dir)
+
+    zip_filename = f'ASSIGNMENT_{hw_number}_{ext_user_username[1:]}.zip'
+    zip_filepath = os.path.join(output_dir, zip_filename)
+
+    if hw_number == '1':
+        random_number = random.randint(1, 9)
+        doc_src = download_from_s3(f'IS100_Assignment{hw_number}_Type{random_number}_Text.docx', temp_dir)
+        pdf_src = download_from_s3(f'IS100_Assignment{hw_number}_Type{random_number}_Question.pdf', temp_dir)
+        doc_dst = f'IS100_Assignment{hw_number}_{ext_user_username[1:]}.docx'
+        pdf_dst = f'IS100_Assignment{hw_number}_Question.pdf'
+        new_docx_path = os.path.join(output_dir, f'IS100_Assignment{hw_number}_{ext_user_username[1:]}.docx')
+
+        modified_docx_path = embed_hidden_info_docx(doc_src, ext_user_username, temp_dir, new_docx_path)
+
+        with zipfile.ZipFile(zip_filepath, 'w') as zipf:
+            zipf.write(modified_docx_path, doc_dst)
+            zipf.write(pdf_src, pdf_dst)
+
+        os.remove(modified_docx_path)
+
+    elif hw_number == '2':
+        random_number = random.randint(1, 2)
+        xlsx_src = download_from_s3(f'IS100_Assignment{hw_number}_Type{random_number}_Question.xlsx', temp_dir)
+        txt_src = download_from_s3(f'IS100_Assignment{hw_number}_Type{random_number}_Data.txt', temp_dir)
+        xlsx_dst = f'IS100_Assignment{hw_number}_{ext_user_username[1:]}.xlsx'
+        txt_dst = f'IS100_Assignment{hw_number}_Data.txt'
+        new_xlsx_path = os.path.join(output_dir, f'IS100_Assignment{hw_number}_{ext_user_username[1:]}.xlsx')
+
+        modified_xlsx_path = embed_hidden_info_xlsx(xlsx_src, ext_user_username, temp_dir, new_xlsx_path)
+
+        with zipfile.ZipFile(zip_filepath, 'w') as zipf:
+            zipf.write(modified_xlsx_path, xlsx_dst)
+            zipf.write(txt_src, txt_dst)
+
+        os.remove(modified_xlsx_path)
+
+    else:
+        print("Invalid hw_number. Please provide either '1' or '2'.")
+        return
+    
+    upload_to_s3(zip_filepath, f'ASSIGNMENT_{hw_number}_{ext_user_username[1:]}.zip')
+    shutil.rmtree(output_dir)
+
+def download_from_s3(key, temp_dir):
+    file_path = os.path.join(temp_dir, key)
+    s3_client.download_file(S3_BUCKET, key, file_path)
+    return file_path
+
+def upload_to_s3(file_path, key):
+    s3_client.upload_file(file_path, S3_BUCKET, key)
+
 @app.route('/initiate-download', methods=['POST'])
 def initiate_download():
     #return request.form
@@ -44,7 +162,7 @@ def initiate_download():
         return jsonify({"error": "Assignment number not correctly provided"}), 400
     
     ext_user_username = request.form.get('ext_user_username')
-    # ext_user_username = "269694"  # Use for debugging purposes
+    # ext_user_username = "e269694"  # Use for debugging purposes
     
     if not ext_user_username:
         return jsonify({"error": "Username not provided"}), 400
@@ -54,6 +172,12 @@ def initiate_download():
     if not re.match(r'^e\d+$', ext_user_username):
         return jsonify({"error": "Invalid username format"}), 400
     
+    temp_dir = tempfile.mkdtemp()
+    try:
+        create_zip(ext_user_username, hw_number, temp_dir)
+    finally:
+        shutil.rmtree(temp_dir)
+
     filename = f'ASSIGNMENT_{hw_number}_{ext_user_username[1:]}.zip'
     session['filename'] = filename
 
